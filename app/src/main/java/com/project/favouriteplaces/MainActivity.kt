d12fa47c9666b579b46884d0b9820fedfefe402c
DinuBotan <dinubotan@gmail.com>
2021-03-05 01:30:52 +0100

    Added ability to take pictures

--- app/src/main/java/com/project/favouriteplaces/MainActivity.kt
+++ app/src/main/java/com/project/favouriteplaces/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.project.favouriteplaces
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        //For permission code
+        private const val CAMERA_PERMISSION_CODE = 1
+        //For intent
+        private const val CAMERA_REQUEST_CODE = 2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,8 +35,46 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btn_camera.setOnClickListener{
+            //Check if we have permissions
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED)
+            {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+            }
+        }
 
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == CAMERA_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                Toast.makeText(this, "Permission was denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    //Automatically called when user makes an image with camera
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == CAMERA_REQUEST_CODE){
+                val thumBnail: Bitmap = data!!.extras!!.get("data") as Bitmap
+                iv_image.setImageBitmap(thumBnail)
+            }
+        }
 
 
     }
